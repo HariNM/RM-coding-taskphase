810dd0c497d3fc17c247b253a5f5bdd1b763eb27
HariNM <86195172+HariNM@users.noreply.github.com>
2022-06-21 03:40:14 +0530

    Link
    https://www.tinkercad.com/things/7gBimb55pGR?sharecode=p7z3fiovaHVhv_fn_TA049gcYpIBVQHu--5LwNNjT2g

---
+++ 5.ino
@@ -0,0 +1,20 @@
+//Ultrasonic sensor 
+int trigpin=12,echopin=11,traveltime;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(trigpin,OUTPUT);
+  pinMode(echopin,INPUT);
+}
+
+void loop()
+{
+  digitalWrite(trigpin,LOW);
+  delayMicroseconds(10);
+  digitalWrite(trigpin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigpin,LOW);
+  traveltime=pulseIn(echopin,HIGH);
+  delay(25);
+  Serial.println(traveltime);
+}
