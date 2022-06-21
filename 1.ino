7ea3e740b82c8a9b98556048eb0761fbc5861250
HariNM <86195172+HariNM@users.noreply.github.com>
2022-06-21 22:19:36 +0530

    link
    https://www.tinkercad.com/things/gS3uPdL52Cu?sharecode=-I4fmowZxSlEispnlWuqRf6TEXhnNRGgrlT7Hbc868U

--- 1.ino
+++ 1.ino
@@ -1,23 +1,25 @@
-//using Arduino blink led for 1,2,3 seconds and loop
 /*
-
+  This program blinks pin 12 of the Arduino (the
+  built-in LED) for 1,2,3 seconds
 */
 
-void setup() {
-    pinMode(13,OUTPUT);
+void setup()
+{
+  pinMode(12, OUTPUT);
 }
 
-void loop() {
-    digitalWrite(13,HIGH);
-    delay(1000);
-    digitalWrite(13,HIGH);
-    delay(1000);
-    digitalWrite(13,HIGH);
-    delay(2000);
-    digitalWrite(13,HIGH);
-    delay(2000);
-    digitalWrite(13,HIGH);
-    delay(3000);
-    digitalWrite(13,HIGH);
-    delay(3000);
+void loop()
+{
+  digitalWrite(12, HIGH);
+  delay(1000);
+  digitalWrite(12, LOW);
+  delay(1000);
+  digitalWrite(12, HIGH);
+  delay(2000);
+  digitalWrite(12, LOW);
+  delay(2000);
+  digitalWrite(12, HIGH);
+  delay(3000);
+  digitalWrite(12, LOW);
+  delay(3000);
 }
